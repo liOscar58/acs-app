0072e46d89e8c1cb277184ca79971693912935f9
Oscar Li <lioscar58@gmail.com>
2023-03-04 20:27:17 -0500

    buttons

--- src/components/colorfinder.js
+++ src/components/colorfinder.js
@@ -1,22 +1,36 @@
+
 // using thecolorapi.com to get the color of the input rgb value */
+
 
 import React, { useState } from "react";
 import "../App.css";
 
+
 function ColorFinder() {
+    //added
+    const msg = new SpeechSynthesisUtterance()
     const [color, setColor] = useState("");
     const [colorName, setColorName] = useState("");
-    
+    const [inputColour, setinputColour] = useState("");
+
     const colorHandler = (e) => {
         setColor(e.target.value);
     };
+
+    const speechHandler = (msg) => {
+        msg.text = inputColour
+        window.speechSynthesis.speak(msg)
+      }
     
     const colorNameHandler = () => {
         fetch(`https://www.thecolorapi.com/id?rgb=${color}`)
         .then((res) => res.json())
         .then((data) => {
             setColorName(data.name.value);
+            setinputColour(data.name.value);
         });
+        speechHandler(msg)
+
     };
     
     return (
@@ -28,7 +42,7 @@ function ColorFinder() {
             placeholder="Enter RGB Value"
             onChange={colorHandler}
         />
-        <button onClick={colorNameHandler}>FIND COLOR</button>
+        <button onClick={colorNameHandler}>FIND COLOR</button>   
         <h2>{colorName}</h2>
         </div>
     );
